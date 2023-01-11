0d814de24be7abdfe43710a2c81e00fda45e7c9f
ShueiYang <yangshuei83@gmail.com>
2023-01-11 01:37:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nasa Mission Control Project
+
+Nasa project learning on the Backend Node.js from ZTM.
+
+Working on Express, Mongoose and MongoDB to connect to the frontend with React.js
---
+++ server/src/services/mongo.js
@@ -0,0 +1,29 @@
+
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB connection ready!");
+});
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function mongoConnect() {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DATABASE_ACCESS}`)  
+        
+    } catch (err) {
+        console.log(err)
+    }
+}; 
+
+async function mongoDisconnect() {
+    await mongoose.disconnect();  
+};
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+};
